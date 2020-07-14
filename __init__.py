4ddff639d8ccce2af59904f418e7b40e3be5bcf0
jamesmf <jamesmfrick@gmail.com>
2020-07-13 23:59:01 -0400

    debug

--- __init__.py
+++ __init__.py
@@ -13,8 +13,12 @@ from mycroft.skills.common_iot_skill import (
 from mycroft.skills.core import FallbackSkill
 from mycroft.util.log import getLogger
 
+LOG = getLogger()
+LOG.INFO("BEGUN")
+
 from .mozilla_client import MozillaIoTClient
 
+LOG.INFO("MADE IT PAST LOAD")
 
 _MAX_BRIGHTNESS = 254
 
@@ -24,16 +28,19 @@ class MozillaIoTSkill(CommonIoTSkill, FallbackSkill):
         super().__init__(name="MozillaIoTSkill")
         self._client: MozillaIoTClient = None
         self._entities = dict()
+        self._scenes: List[str] = []
+        LOG.INFO("init")
 
     def initialize(self):
         self.settings.set_changed_callback(self.on_websettings_changed)
         self._setup()
         self._entities: List[str] = self._client.entity_names
         self._scenes = []
+        LOG.INFO("ABOUT TO REGISTER")
         self.register_entities_and_scenes()
 
     def _setup(self):
-        self.client = MozillaIoTClient(
+        self._client = MozillaIoTClient(
             token=self.settings.get("token"), host=self.settings.get("host")
         )
 
