8eaa73109948b98809c21b8893d81ee50ca13222
jamesmf <jamesmfrick@gmail.com>
2020-07-14 00:07:25 -0400

    debug

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from typing import List
+import requests
 
 from mycroft.skills.common_iot_skill import (
     CommonIoTSkill,
@@ -16,11 +17,42 @@ from mycroft.util.log import getLogger
 LOG = getLogger()
 LOG.INFO("BEGUN")
 
-from .mozilla_client import MozillaIoTClient
+# from .mozilla_client import MozillaIoTClient
 
 LOG.INFO("MADE IT PAST LOAD")
 
 _MAX_BRIGHTNESS = 254
+
+
+class MozillaIoTClient:
+    def __init__(self, host: str, token: str):
+        """
+        Client for interacting with the Mozilla IoT API
+        """
+        self.host = host
+        self.headers = {
+            "Authorization": "Bearer {}".format(token),
+            "Content-Type": "application/json",
+        }
+        self.things = self.get_things()
+        self.entity_names: List[str] = [
+            thing["title"] for thing in self.things if "title" in thing
+        ]
+
+    def _request(self, method: str, endpoint: str, data: dict = None):
+
+        url = self.host + endpoint
+
+        response = requests.request(method, url, json=data, headers=self.headers)
+
+        response.raise_for_status()
+
+        return response
+
+    def get_things(self):
+        if self.host:
+            return self._request("GET", "/things/")
+        return []
 
 
 class MozillaIoTSkill(CommonIoTSkill, FallbackSkill):
