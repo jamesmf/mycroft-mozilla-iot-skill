fc45172cea0cdc5f97ac97d24c995540db5e45bf
jamesmf <jamesmfrick@gmail.com>
2020-08-21 00:05:52 -0400

    synonyms and on/off

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import List, Dict, Union, Optional
+from typing import List, Dict, Union, Optional, Any
 import requests
 import json
 
@@ -17,12 +17,38 @@ from mycroft.util.log import getLogger
 
 LOG = getLogger()
 
+ThingType = Dict[Any, Any]  # replace if there's a good type for a Thing response
 
-_MAX_BRIGHTNESS = 254
+
+# map to help to convert some actions to "set" requests on properties
+ACTION_TO_PROPERTY = {Action.ON: "on", Action.OFF: "off"}
+
+# actions that are "set" type actions
+SET_ACTIONS = (Action.SET, Action.ON, Action.OFF)
+
+# synonyms
+SYNONYMS = [("light", "lamp", "lights")]
 
 
 def normalize(name: str) -> str:
     return name.lower().strip()
+
+
+def get_alternate_names(name: str) -> List[str]:
+    """
+    replace words with synonyms to create alternate names. This should allow
+    for flexibility in naming of Things
+    """
+    sp = name.split(" ")
+    out = []
+    for synset in SYNONYMS:
+        for word in synset:
+            if word in sp:
+                for replacement in [w for w in synset if w != word]:
+                    other = sp.copy()
+                    other[other.index(word)] = replacement
+                    out.append(normalize(" ".join(other)))
+    return out
 
 
 class MozillaIoTClient:
@@ -40,12 +66,17 @@ class MozillaIoTClient:
         }
         LOG.info("client get_things()")
         self.things = self.get_things()
-        self.entity_names: List[str] = [
-            normalize(thing["title"]) for thing in self.things if "title" in thing
-        ]
+        self.entity_names: Dict[str, ThingType] = {}
+        for thing in self.things:
+            name = normalize(thing["title"])
+            self.entity_names[name] = thing
+            # add synonyms if relevant
+            alternates = get_alternate_names(name)
+            for other_name in alternates:
+                self.entity_names[other_name] = thing
         LOG.info("finished client init")
 
-    def _request(self, method: str, endpoint: str, data: dict = None):
+    def request(self, method: str, endpoint: str, data: dict = None):
 
         url = self.host + endpoint
         LOG.info(url)
@@ -59,27 +90,34 @@ class MozillaIoTClient:
 
     def get_things(self):
         if self.host:
-            resp = self._request("GET", "/things/")
+            resp = self.request("GET", "/things/")
             print(resp)
             return resp.json()
         return []
 
+    def resolve_entity(self, entity_value: str) -> ThingType:
+        """
+        Given an input entity value from the NLU, return the a matched
+        name if available. For instance, if the resolved entity is
+        "kitchen lights" but the closest Thing is "kitchen light" return
+        the kitchen light Thing
+        """
+        thing = self.entity_names.get(normalize(entity_value), None)
+        LOG.info(json.dumps(thing, indent=2))
+        return thing
+
     def get_set_value_request(
-        self, entity: str, attribute: Optional[str], value: Union[str, int, float, None]
+        self,
+        thing: ThingType,
+        attribute: Optional[str],
+        value: Union[str, int, float, None],
     ):
         """
         Attempt to set a thing's property value
         """
-        things = [th for th in self.things if normalize(th["title"]) == entity]
-        if len(things) == 0:
-            return False, "", {}
-        thing = things[0]
-        LOG.info(json.dumps(thing, indent=2))
-        # assume if we have no attribute that we're talking about a 'level' (for now)
-        if attribute is None:
-            attribute = "level"
+
         for prop, prop_dict in thing.get("properties", {}).items():
-            LOG.info(f"considering {prop}")
+            LOG.info(f"considering {prop}, comparing to {attribute}")
             if attribute in (normalize(prop), normalize(prop_dict["title"])):
                 LOG.info(f"matched {prop}")
                 url = prop_dict.get("links", [{}])[0].get("href", "")
@@ -95,7 +133,7 @@ class MozillaIoTSkill(CommonIoTSkill, FallbackSkill):
         super().__init__(name="MozillaIoTSkill")
 
         self._client: MozillaIoTClient = None
-        self._entities = []
+        self._entities: List[ThingType] = []
         self._scenes: List[str] = []
 
     def initialize(self):
@@ -127,19 +165,29 @@ class MozillaIoTSkill(CommonIoTSkill, FallbackSkill):
     def supported_request_version(self) -> IoTRequestVersion:
         return IoTRequestVersion.V3
 
+    def resolve_nicknames(self, name: str) -> str:
+        """
+        Allow for creating a nickname mapping
+
+        TODO: implement
+        """
+        return name
+
     def can_handle(self, request):
         LOG.info("Mozilla IoT was consulted")
         LOG.info(request)
         can_handle = False
         callback = {}
-        if request.action == Action.SET and request.entity in self._entities:
+        entity_name = self.resolve_nicknames(request.entity)
+        thing = self._client.resolve_entity(entity_name)
+        if thing is None:
+            LOG.info(f"mozilla-iot: no such thing as {request.entity}")
+            return False, {}
+        if request.action in SET_ACTIONS:
+            # 'on/off' action and the like become 'set' of a property
+            attribute = ACTION_TO_PROPERTY.get(request.action, request.attribute)
             can_handle, callback = self._client.get_set_value_request(
-                request.entity, request.attribute, request.value
-            )
-        else:
-            LOG.info(f"mozilla-iot: cannot handle {request.action}, {request.entity}")
-            LOG.info(
-                f"mozilla-iot: cannot handle {request.action == Action.SET}, {request.entity in self._entities}"
+                thing, attribute, request.value
             )
 
         return can_handle, callback
@@ -147,7 +195,7 @@ class MozillaIoTSkill(CommonIoTSkill, FallbackSkill):
     def run_request(self, request, cb):
         LOG.info(str(request), request)
         LOG.info(cb)
-        self._client._request(cb["method"], cb["url"], cb["data"])
+        self._client.request(cb["method"], cb["url"], cb["data"])
 
 
 def create_skill():
